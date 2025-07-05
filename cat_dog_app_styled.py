6614dba111069f3ebb57485a88f3948ae8838629
Harshit Pandiyar <149085805+Harshit1983@users.noreply.github.com>
2025-07-05 13:51:35 +0530

    cat_dog_app_styled.py

--- cat_dog_app_styled.py
+++ cat_dog_app_styled.py
@@ -66,11 +66,11 @@ if uploaded_file:
     confidence = model.decision_function(features)[0]
     margin = abs(confidence)
 
-    # if margin < 0.1:
-    #     st.warning("⚠️ Hmm... this image doesn't look like a cat or dog.\nAre you trying to test Shree? 😅")
-    #     st.info("Tip: Please upload a **clear cat or dog image** for accurate prediction.")
-    # else:
-    if prediction == 0:
-        st.success("😺 It's a **Cat**!")
-    elif prediction == 1:
-        st.success("🐶 It's a **Dog**!")
+    if margin < 0.1:
+        st.warning("⚠️ this image doesn't look like a cat or dog.")
+        st.info("Tip: Please upload a **clear cat or dog image** for accurate prediction.")
+    else:
+        if prediction == 0:
+            st.success("😺 It's a **Cat**!")
+        elif prediction == 1:
+            st.success("🐶 It's a **Dog**!")
